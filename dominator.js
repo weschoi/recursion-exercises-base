ec1051f00fb3770dc261e79d9753be3a92bfc297
Wesley Choi <weschoi.creative@gmail.com>
2017-05-11 22:34:11 -0700

    make all tests pass in dominator.js

--- dominator.js
+++ dominator.js
@@ -2,16 +2,51 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+
+  const tree = flattenTreeToArray(root);
+
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].id === id) {
+      return tree[i]
+    }
+  }
+
 };
+
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const tree = flattenTreeToArray(root);
+
+  let result = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].className) {
+      let classes = (tree[i].className).split(' ');
+      for (let j = 0; j < classes.length; j++) {
+        if (classes[j] === className) {
+          result.push(tree[i])
+        }
+      }
+    }
+  }
+  return result
 };
 
+
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const tree = flattenTreeToArray(root);
+
+  let result = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].tagName === tagName) {
+      result.push(tree[i])
+    }
+  }
+
+  return result
 };
+
 
 module.exports = {
   getElementById: getElementById,
